7ff6afe954e42f4406ffb3cd2392216868d6f0c5
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-13 19:38:47 +0100

    v3 test

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://economista-portfolio-server.vercel.app",
   withCredentials: true, // <— envia cookies
   timeout: 10000,
 });
