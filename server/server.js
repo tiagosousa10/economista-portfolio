f5aed60bb7caf94d2c2aaff7101b9b9148d7700a
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-20 00:59:45 +0100

    cors v3

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
 };
 
 //middlewares
